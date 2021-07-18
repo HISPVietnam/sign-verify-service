c8926779e8c9a7c99ea204df7b8ef2f1fe80df51
Morten Hansen <morten@winterop.com>
2021-07-18 17:43:24 +0700

    feat: prettyPrint console output

--- logger.js
+++ logger.js
@@ -2,11 +2,12 @@ const fs = require("fs");
 const path = require("path");
 const multistream = require("pino-multi-stream").multistream;
 
-const createLogger = ({ name }) => {
+const createLogger = ({ name, prettyPrint = true }) => {
   return require("pino")(
     {
       name,
       level: "info",
+      prettyPrint,
     },
     multistream([
       { stream: process.stdout, prettyPrint: false },
