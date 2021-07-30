a58d133ec8b7506bcb38a74c12c14ed18abd3e2f
Morten Hansen <morten@winterop.com>
2021-07-30 10:05:43 +0200

    doc: minor csca/dsc comments

--- scripts/gen-csca.sh
+++ scripts/gen-csca.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# ES256 = P-256 EC + SHA256
+
 openssl ecparam -name prime256v1 -genkey -noout -out csca.key
-openssl req -x509 -key csca.key -days 365 -subj "/C=NO/CN=My Certificate Authority" -out csca.pem -nodes -new
+openssl req -x509 -key csca.key -days 365 -subj "/C=VN/CN=HISP Vietnam CA" \
+    -out csca.pem -nodes -new
--- scripts/gen-dsc.sh
+++ scripts/gen-dsc.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+# ES256 = P-256 EC + SHA256
+
 openssl ecparam -name prime256v1 -genkey -noout -out dsc-$1.key
-openssl req -new -subj "/C=NO/CN=My DSC $1" -key dsc-$1.key -nodes |
+openssl req -new -subj "/C=VN/CN=HISP Vietnam DSC $1" -key dsc-$1.key -nodes |
     openssl x509 -req -CA csca.pem -CAkey csca.key -set_serial 0x1 -days 365 -out dsc-$1.pem
 openssl pkcs8 -in dsc-$1.key -nocrypt -topk8 -out dsc-$1.p8
