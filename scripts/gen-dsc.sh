47bb4844e020cb0fea8d6d2a0d34515eb74d0dd1
Morten Hansen <morten@winterop.com>
2021-08-11 12:38:42 +0200

    chore: remove old openssl scripts

--- scripts/gen-dsc.sh
+++ scripts/gen-dsc.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+set -e
+
+# https://jmrtd.org/certificates.shtml
+# CSCA = Country Signing Certificate Authority
+# CSC = Country Signing Certificate
+# DSC = Document Signing Certificate
+#
 # ES256 = P-256 EC + SHA256
 
 openssl ecparam -name prime256v1 -genkey -noout -out dsc-$1.key
