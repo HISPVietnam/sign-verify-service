19baae7a60366ecd026cbb9514ab7d1209a92cc9
Morten Hansen <morten@winterop.com>
2021-08-27 15:50:18 +0200

    feat: add back package for supporting both sign/verify (useful for testing)

---
+++ packages/svs-http-api-full/lib/index.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021, HISP Vietnam, Co.Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+const { createLogger } = require("@svs/core/lib/logger");
+const createSecurity = require("@svs/core/lib/security");
+const createSchemaValidator = require("@svs/core/lib/schema");
+const createHttpClient = require("@svs/core/lib/http-client");
+const { startServer } = require("./server");
+
+const cfg = require("@svs/core/lib/config")("svs.yml");
+
+startServer({
+  cfg,
+  security: createSecurity(cfg),
+  schemaValidator: createSchemaValidator(cfg.schema),
+  logger: createLogger({ name: "svs" }),
+  httpClient: createHttpClient(cfg.httpClient),
+});
